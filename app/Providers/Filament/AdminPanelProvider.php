fe379f57e6d160d7b407e291f5e4c351f3599255
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-06-05 00:00:09 +0500

    updated files

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,8 @@ class AdminPanelProvider extends PanelProvider
             ->path('')
             ->authGuard('web')
             ->passwordReset()
+            ->brandLogo(asset('images/logo.png'))
+            ->brandLogoHeight('3rem')
             ->login()
             ->defaultThemeMode(ThemeMode::Light)
             ->colors([
